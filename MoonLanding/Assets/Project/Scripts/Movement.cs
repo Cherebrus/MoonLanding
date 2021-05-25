0c335fe7f60149b1119b973bb1a6e432404f15a6
Cherebrus <74614035+Cherebrus@users.noreply.github.com>
2021-05-25 17:10:53 +0300

    1
    1

---
+++ MoonLanding/Assets/Project/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class Movement : MonoBehaviour
+    {
+        private float _horizontal;
+        private float _vertical;
+
+        private float _xMovement;
+        private float _yMovement;
+        
+        private int _speed = 10;
+
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Update()
+        {
+            Inpts();
+        }
+        
+        private void Inpts()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+            _vertical = Input.GetAxis("Vertical");
+        }
+
+        private void Move()
+        {
+            _xMovement = _horizontal * _speed * Time.deltaTime;
+            _yMovement = _vertical * _speed * Time.deltaTime;
+
+        }
+
+        
+    }
+}
